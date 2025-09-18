75bf887bd9942f35e4ca1d66687e970235667247
Abu-Farhad <abufarhadsardar91@gmail.com>
2025-09-18 22:53:49 +0530

    update

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(express.json())
 app.use(cors({
   origin: allowedOrigins,   // <-- let cors handle the matching
   methods: ['GET','POST','PUT','DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
