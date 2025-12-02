8fc2cb6a7c3f2c6f6335938fea153de19cc5139c
Abu-Farhad <abufarhadsardar91@gmail.com>
2025-12-02 08:34:20 +0530

    email readd

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,42 +8,47 @@ import appointmentModel from '../models/appointmentModel.js'
 import Razorpay from 'razorpay'
 
 // API to register user
-const registerUser=async(req,res)=>{
-    try {
-        const {name,password,phone}=req.body
-        if(!name||!password ||!phone){
-            return res.json({success:false,message:"Missing Details"})
-        }
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, phone } = req.body;
 
-        // validating email format
-        if(!validator.isEmail(email)){
-            return res.json({success:false,message:"Enter a valid email"})
-        }
-
-        //validating strong password
-        if(password.length<8){
-            return res.json({success:false,message:"Enter a strong password"})
-        }
-
-        // hashing user password
-        const salt =await bcrypt.genSalt(10)
-        const hashedPassword=await bcrypt.hash(password,salt)
-
-        const userData={
-            name,email,password:hashedPassword,phone
-        }
-
-        const newUser=new userModel(userData)
-        const user=await newUser.save();
-
-        const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
-
-        res.json({success:true,token})
-    } catch (error) {
-     console.log(error)
-     res.json({success:false,message:error.message})   
+    if (!name || !email || !password || !phone) {
+      return res.json({ success: false, message: "Missing Details" });
     }
-}
+
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Enter a valid email" });
+    }
+
+    // strong password
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter a strong password" });
+    }
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      phone,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 
 //API for user login
 const loginUser=async(req,res)=>{
